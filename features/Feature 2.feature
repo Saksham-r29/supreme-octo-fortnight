6d6e2d4079a6f57f03ad145cb3089c369a5eead6
saksham.raizada@blinq.io <Saksham Raizada>
2025-12-15 05:21:20 +0000

    Saksham Raizada pushed changes to main branch from recorder

--- features/Feature 2.feature
+++ features/Feature 2.feature
@@ -1,3 +1,6 @@
 Feature: Feature 2
-	@regression
-	Scenario Outline: Scenario 1
+
+  @regression
+  Scenario: Scenario 1
+    # Click on Username, Click on Username *Password *Login
+    Given The user navigates to the login page and interacts with the Username and Password fields
--- features/step_definitions/login_page.mjs
+++ features/step_definitions/login_page.mjs
@@ -3,7 +3,9 @@ import { closeContext, initContext, navigate, TestContext as context } from "aut
 
 const elements = {
   paragraph_blinq_user:{"locators":[{"css":"internal:text=\"blinq_user\"s","priority":1},{"css":"internal:text=\"blinq_user\"i","priority":1},{"css":"p >> internal:has-text=\"blinq_user\"i","priority":1},{"css":"p >> internal:has-text=/^blinq_user$/","priority":1},{"css":"internal:role=paragraph >> internal:has-text=\"blinq_user\"i","priority":1}],"element_name":"blinq_user paragraph","element_key":"paragraph_blinq_user"},
-  textbox_password:{"locators":[{"css":"internal:role=textbox[name=\"Password\"i]","priority":1},{"css":"internal:role=textbox[name=\"Password\"s]","priority":1}],"element_name":"Password textbox","element_key":"textbox_password"}
+  textbox_password:{"locators":[{"css":"internal:role=textbox[name=\"Password\"i]","priority":1},{"css":"internal:role=textbox[name=\"Password\"s]","priority":1}],"element_name":"Password textbox","element_key":"textbox_password"},
+  textbox_username:{"locators":[{"css":"internal:role=textbox[name=\"Username\"i]","priority":1},{"css":"internal:role=textbox[name=\"Username\"s]","priority":1}],"element_name":"Username textbox","element_key":"textbox_username"},
+  text_username_password_login:{"locators":[{"css":"internal:text=\"Username *Password *Login\"i","priority":1},{"css":"internal:text=\"Username *Password *Login\"s","priority":1},{"css":"form >> internal:has-text=\"Username *Password *Login\"i","priority":1}],"element_name":"Username *Password *Login Text","element_key":"text_username_password_login"}
 };
 
 /**
@@ -35,3 +37,20 @@ async function the_user_navigates_to_the_login_page_and_clicks_on_the_password_t
 }
 
 Given("The user navigates to the login page and clicks on the Password textbox", { timeout: 60000}, the_user_navigates_to_the_login_page_and_clicks_on_the_password_textbox);
+
+/**
+ * The user navigates to the login page and interacts with the Username and Password fields
+ * @recorder
+ * @path=/login
+ */
+async function the_user_navigates_to_the_login_page_and_interacts_with_the_username_and_password_fields (){
+// source: recorder
+// implemented_at: 2025-12-15T05:21:19.900Z
+  const _params = {  };
+  // Click on Username textbox
+  await context.web.click(elements["textbox_username"], _params, null, this);
+  // Click on Username *Password *Login Text
+  await context.web.click(elements["text_username_password_login"], _params, null, this);
+}
+
+Given("The user navigates to the login page and interacts with the Username and Password fields", { timeout: 120000}, the_user_navigates_to_the_login_page_and_interacts_with_the_username_and_password_fields);
